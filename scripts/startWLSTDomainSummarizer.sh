f003ce3130c2125bf8d5fd8ad5596bde2b7cc5ba
Filipe Rosset <rosset.filipe@gmail.com>
2014-06-27 11:10:07 -0300

    fix paths to match my environment configuration

--- scripts/startWLSTDomainSummarizer.sh
+++ scripts/startWLSTDomainSummarizer.sh
@@ -6,9 +6,11 @@
 #The WLST_OUTPUT_PATH directory value must have a trailing slash. If there is no trailing slash 
 #script will error and not continue.
 
-WL_HOME=/oracle/middleware/wlserver_10.3
-DOMAIN_HOME=/oracle/middleware/user_projects/domains/MyDomain; export DOMAIN_HOME
-WLST_OUTPUT_PATH=/oracle/WLSTDomainSummarizer/output/; export WLST_OUTPUT_PATH
-WLST_OUTPUT_FILE=WLST_MBean_Config_Summary.html; export WLST_OUTPUT_FILE
+#my configuration
+export domain_name=medrec
+WL_HOME=/oracle/product/Middleware/11g/wlserver_10.3
+DOMAIN_HOME=/oracle/product/Middleware/11g/wlserver_10.3/samples/domains/$domain_name; export DOMAIN_HOME
+WLST_OUTPUT_PATH=/oracle/scripts/WLSTDomainSummarizer/output/; export WLST_OUTPUT_PATH
+WLST_OUTPUT_FILE=WLST_MBean_Config_Summary_domain_$domain_name.html; export WLST_OUTPUT_FILE
 
 ${WL_HOME}/common/bin/wlst.sh StartDomainSummarizer.py
